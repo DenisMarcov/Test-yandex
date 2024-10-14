8fb1c7b3237fdf9ff44516b82bd3bb73423f0619
DenisMarcov <95167521+DenisMarcov@users.noreply.github.com>
2024-10-15 01:17:27 +0400

    Проект готов (bem исправлен, ПП ровный, слайдеры работают, шрифты предзагружаются)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,55 +2,81 @@ let swiperPlans;
 let swiperPlayers;
 
 function initSwiperPlans() {
-    if (window.innerWidth < 1366) {
-        if (!swiperPlans) {
-            swiperPlans = new Swiper('.swiper-plans', {
-                navigation: {
-                    nextEl: '.swiper-plans .swiper-button-next',
-                    prevEl: '.swiper-plans .swiper-button-prev',
-                },
-                pagination: {
-                    el: '.swiper-plans .swiper-pagination',
-                    clickable: true,
-                },
-                speed: 400,
-                spaceBetween: 100,
-            });
-        }
-    } else if (swiperPlans) {
-        swiperPlans.destroy(false, true); 
-        swiperPlans = undefined;
+  if (window.innerWidth < 1366) {
+    if (!swiperPlans) {
+      swiperPlans = new Swiper('.swiper-plans', {
+        navigation: {
+          nextEl: '.swiper-plans .swiper-button-next',
+          prevEl: '.swiper-plans .swiper-button-prev',
+        },
+        pagination: {
+          el: '.swiper-plans .swiper-pagination',
+          clickable: true,
+        },
+        speed: 400,
+        spaceBetween: 100,
+      });
     }
+  } else if (swiperPlans) {
+    swiperPlans.destroy(false, true);
+    swiperPlans = undefined;
+  }
 }
 
 swiperPlayers = new Swiper('.swiper-players', {
-    navigation: {
-        nextEl: '.swiper-players .swiper-button-next',
-        prevEl: '.swiper-players .swiper-button-prev',
+  navigation: {
+    nextEl: '.swiper-players .swiper-button-next',
+    prevEl: '.swiper-players .swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-players .swiper-pagination',
+    type: 'fraction',
+    renderFraction: (currentClass, totalClass) => `
+      <span class="${currentClass}"></span><span class="slash-pagination">/</span>
+      <span class="${totalClass}"></span>`,
+  },
+  observer: true,
+  observeParents: true,
+  speed: 400,
+  spaceBetween: 100,
+  on: {
+    init() {
+      setTimeout(updateFraction, 0, this)
     },
-    pagination: {
-        el: '.swiper-players .swiper-pagination',
-        type: 'fraction',
-        
+    slideChange() {
+      updateFraction(this);
     },
-    
-    speed: 400,
-    spaceBetween: 100,
-    breakpoints: {
-        1024: {
-            slidesPerView: 3,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        576: {
-            slidesPerView: 1,
-        }
+    resize() {
+      updateFraction(this);
+    },
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    576: {
+      slidesPerView: 1,
     }
+  }
 });
 
 initSwiperPlans();
 
-window.addEventListener('resize', function() {
-    initSwiperPlans();
+window.addEventListener('resize', function () {
+  initSwiperPlans();
 });
+
+function updateFraction(slider) {
+  const { params, activeIndex } = slider;
+  const currentElement = slider.pagination.el.querySelector(`.${params.pagination.currentClass}`);
+  const totalElement = slider.pagination.el.querySelector(`.${params.pagination.totalClass}`);
+
+  if (currentElement && totalElement) {
+    const totalSlides = slider.slides.length;
+    currentElement.textContent = `${activeIndex + params.slidesPerView}`;
+    totalElement.textContent = `${totalSlides}`;
+  }
+}
